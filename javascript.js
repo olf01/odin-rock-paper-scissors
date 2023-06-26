70609243f9e797e706b1f8b68c28eb65c060a269
Oscar F <[200~133872824+olf01@users.noreply.github.com>
2023-06-26 12:05:10 +0100

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,23 @@
 function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
-    let choice = options[Math.floor(Math.random() * 3)]
-    return choice
+    let choice = options[Math.floor(Math.random() * 3)];
+    return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "draw";
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "win";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "win";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "win";
+    }
+    else {
+        return "lose";
+    }
 }
