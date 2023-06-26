6a566f03963360d0fead133f97be5b7813127936
Oscar F <[200~133872824+olf01@users.noreply.github.com>
2023-06-26 13:29:29 +0100

    Complete game function

--- javascript.js
+++ javascript.js
@@ -4,6 +4,16 @@ function getComputerChoice() {
     return choice;
 }
 
+function getPlayerChoice() {
+    let choice = prompt("Choose rock, paper or scissors").toLowerCase();
+    
+    while (!(["rock", "paper", "scissors"].includes(choice))) {
+        choice = prompt("Choose rock, paper or scissors").toLowerCase();
+    };
+
+    return choice;
+}
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "draw";
@@ -21,3 +31,38 @@ function playRound(playerSelection, computerSelection) {
         return "lose";
     }
 }
+
+function game() {
+    let score = 0
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        let outcome = playRound(playerSelection, computerSelection);
+
+        if (outcome === "win") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+            score += 1
+        }
+        else if (outcome === "draw") {
+            console.log("Draw!")
+        }
+        else {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+            score -= 1
+        }
+    }
+
+    if (score > 0) {
+        console.log("Congratulations! You've won the game!");
+    }
+    else if (score === 0) {
+        console.log("Game draw!")
+    }
+    else {
+        console.log("Oh no! You've lost the game!")
+    }
+
+}
+
+game()
